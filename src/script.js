022098c85970f559e6e29f9ba317deaf1159870c
Megan McKissack <meganmckissack@gmail.com>
2022-10-31 21:40:10 -0700

    update cameras and controls

--- src/script.js
+++ src/script.js
@@ -1,8 +1,27 @@
 import * as THREE from 'three';
-//give us access to all the three.js elements
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+//gives us access to all the three.js elements
 const scene = new THREE.Scene();
 
-//geomertry and mesh
+
+//Cursor
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = - (event.clientY / sizes.height - 0.5); 
+
+  console.log(event.clientX, event.clientY);
+});
+
+//Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+//geometry and mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 0xff0000 });
 
@@ -19,16 +38,42 @@ const sizes = {
 }
 
 //Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000);
 camera.position.z = 3;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
-//Canvas
-const canvas = document.querySelector('canvas.webgl');
+// Controls
+const controls = new OrbitControls(camera, canvas);
+// controls.target.y = 2;
+controls.enableDamping = true; //to make the animation smoother
+
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+
+
+
+//Animation
+const tick = () => {
+  //update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(mesh.position);
+
+  //update controls
+  controls.update();
+
+  // renderer
+  renderer.render(scene, camera);
+
+  // call tick again on the next frame
+  window.requestAnimationFrame(tick);
+}
+
+tick();
