2c29991c63797a054688a2ab849911e895c75054
Megan McKissack <meganmckissack@gmail.com>
2022-10-31 20:07:26 -0700

    create group objects

--- src/script.js
+++ src/script.js
@@ -2,16 +2,54 @@ import * as THREE from 'three';
 //give us access to all the three.js elements
 const scene = new THREE.Scene();
 
+const axesHelper = new THREE.AxesHelper(2);
+
+
 //geomertry and mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000 });
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({color: 0xff0000 });
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+// scene.add(axesHelper);
 
-mesh.position.x = 0.75
-mesh.position.y = - 0.1
-mesh.position.z = 0.3
+// mesh.position.x = 0.75
+// mesh.position.y = - 0.1
+// mesh.position.z = 0.3
+
+// mesh.scale.x = 1;
+// mesh.scale.y = 1;
+// mesh.scale.z = 1;
+
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+cube1.position.x = - 1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+cube3.position.x = 1.5;
+group.add(cube3);
 
 const sizes = {
   width: 800,
@@ -22,6 +60,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
+
+// camera.lookAt(mesh.position);
 
 //Canvas
 const canvas = document.querySelector('canvas.webgl');
